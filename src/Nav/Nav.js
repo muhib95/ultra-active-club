7ea343114e2bbb70152bb0d975c32cac008b632a
Muhibbul Hasan <muhibbulhasan6@gmail.com>
2022-09-29 17:15:47 +0600

    nav logo

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Nav.css';
+import logo from '../image/logo.jpg';
 const Nav = () => {
   return (
     <div className='nav-bar'>
+      <img src={logo} alt="" />
       <h1>ACTIVE-FOR-PROGRAMMING-HERO</h1>
     </div>
   );
